6f1d18c044c1db4b083a132630a4cc4a6912fa74
harsh503 <thadodaharsh10@gmail.com>
2017-10-29 02:46:30 +0530

    db error resolved

--- db/migrate/20170924064614_create_friendships.rb
+++ db/migrate/20170924064614_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[5.1]
   def change
     create_table :friendships do |t|
-      t.string :userid1 ,:null=>false ,
-      t.string :userid2 ,:null=>false ,
+      t.string :userid1 ,:null=>false
+      t.string :userid2 ,:null=>false
       t.integer :status
 
       t.timestamps
